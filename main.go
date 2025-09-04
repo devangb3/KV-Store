1562690e21e8a433d7c70eaa1bb78736ddb4de7c
Devang Borkar <devangborkar3@gmail.com>
2025-09-04 10:58:48 -0700

    Added delete method

--- main.go
+++ main.go
@@ -15,6 +15,10 @@ func handler(w http.ResponseWriter, r *http.Request) {
 var kvStore = make(map[string]string)
 
 func putKvStoreHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost{
+		http.Error(w, "Invalid method, only accepts POST requests", http.StatusMethodNotAllowed);
+		return;
+	}
 	var data struct{
 		Key string `json:"key"`
 		Value string `json:"value`
@@ -22,18 +26,23 @@ func putKvStoreHandler(w http.ResponseWriter, r *http.Request) {
 	
 	err := json.NewDecoder(r.Body).Decode(&data);
 	if err != nil{
-		fmt.Fprintf(w, "Error in decoding!")
+		http.Error(w, "Error occurred during Decoding", http.StatusBadRequest);
 		return
 	}
 	if data.Key == "" || data.Value == ""{
-		fmt.Fprintf(w, "Both key and value should be non-empty");
+		http.Error(w, "Either key or value is empty", http.StatusBadRequest);
 	}
 	kvStore[data.Key] = data.Value;
+	
+	log.Printf("Key : %v stored with value : %v", data.Key, data.Value)
 	fmt.Fprintf(w, "Key : %v stored with value : %v", data.Key, data.Value)
 }
 
 func getKVStoreHandler(w http.ResponseWriter, r *http.Request) {
-	
+	if r.Method != http.MethodGet{
+		http.Error(w,"Invalid method, only accepts GET requests", http.StatusMethodNotAllowed);
+		return;
+	}
 	key := r.URL.Query().Get("key")
 	if key == ""{
 		http.Error(w, "Key parameter empty", http.StatusBadRequest);
@@ -52,9 +61,37 @@ func getKVStoreHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(response);
 }
+
+func deleteKVStoreHandler(w http.ResponseWriter, r *http.Request){
+	if r.Method != http.MethodDelete{
+		http.Error(w, "Invalid Request method", http.StatusMethodNotAllowed)
+		return;
+	}
+	var data struct{
+		Key string `json:"key"`
+	}
+	err := json.NewDecoder(r.Body).Decode(&data);
+	if err != nil{
+		http.Error(w, "Error decoding JSON body", http.StatusBadRequest);
+		return;
+	}
+	if(data.Key == ""){
+		http.Error(w, "Key cannot be empty", http.StatusBadRequest);
+		return;
+	}
+	if _,ok := kvStore[data.Key]; !ok{
+		http.Error(w, "Cannot delete if key does not exist", http.StatusNotFound);
+		return;
+	}
+	delete(kvStore, data.Key);
+	log.Printf("Key Deleted successfully %v", data.Key);
+	fmt.Fprintf(w, "Deleted key %v from map successfully", data.Key);
+}
+
 func main() {
 	http.HandleFunc("/", handler)
 	http.HandleFunc("/put", putKvStoreHandler)
 	http.HandleFunc("/get", getKVStoreHandler)
+	http.HandleFunc("/delete", deleteKVStoreHandler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
